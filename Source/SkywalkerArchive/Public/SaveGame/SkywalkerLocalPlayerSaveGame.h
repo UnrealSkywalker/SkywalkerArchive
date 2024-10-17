90f671cbe24c2980fc2172d14c4991f60f8d1d92
Shyfan <410713847@qq.com>
2024-10-17 20:19:36 +0800

    保存

---
+++ Source/SkywalkerArchive/Public/SaveGame/SkywalkerLocalPlayerSaveGame.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "SkywalkerLocalPlayerSaveGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKYWALKERARCHIVE_API USkywalkerLocalPlayerSaveGame : public ULocalPlayerSaveGame
+{
+	GENERATED_BODY()
+	
+};
